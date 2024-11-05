7758490fb4c758e0be49bbfc092595c4be12a135
Alex Schloegl <alexander.schloegl@compass.at>
2024-11-05 15:06:06 +0100

    New channel: Microsoft power automate

---
+++ src/Channel/Handler/MicrosoftPowerAutomate.php
@@ -0,0 +1,54 @@
+<?php
+namespace Notification\Channel\Handler;
+
+class MicrosoftPowerAutomate implements Handler
+{
+	const MAXIMUM_TEXT_LENGTH = 6000;
+
+	public function handle(HandleData $data): void
+	{
+		$options = $data->getOptions();
+		$content = $data->getData();
+
+		foreach ($content as $item)
+		{
+			if (!($text = $this->sanitizeText($item)))
+			{
+				continue;
+			}
+
+			$ch = curl_init();
+
+			$body = json_encode([
+				'text' => $text,
+			]);
+
+			curl_setopt($ch, CURLOPT_URL, $options['hook']);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt(
+				$ch,
+				CURLOPT_HTTPHEADER,
+				[
+					'Content-Type: application/json',
+				]
+			);
+			curl_setopt(
+				$ch,
+				CURLOPT_POSTFIELDS,
+				$body
+			);
+
+			curl_exec($ch);
+
+			curl_close($ch);
+		}
+	}
+
+	private function sanitizeText(string $text): string
+	{
+		return strlen($text) > self::MAXIMUM_TEXT_LENGTH
+			? substr($text, 0, self::MAXIMUM_TEXT_LENGTH - 3) . '...'
+			: $text;
+	}
+}
